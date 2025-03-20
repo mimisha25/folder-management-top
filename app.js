e2f8e535777e2e7c4604caca56992c3eaaa38300
mimisha25 <49067055+mimisha25@users.noreply.github.com>
2025-03-20 14:28:40 -0400

    Uncomment folder route

--- app.js
+++ app.js
@@ -54,13 +54,13 @@ app.use((err, req, res, next) => {
 })
 
 const dashboardRouter = require('./routes/dashboard');
-// const foldersRouter = require('./routes/folder');
+const foldersRouter = require('./routes/folder');
 // const filesRouter = require('./routes/files');
 // const homeRouter = require('./routes/home');
 // const uploadRouter = require('./routes/upload');
 
 app.use('/', dashboardRouter);
-// app.use('/', foldersRouter);
+app.use('/', foldersRouter);
 // app.use('/', filesRouter);
 // app.use('/', homeRouter);
 // app.use('/', uploadRouter);
