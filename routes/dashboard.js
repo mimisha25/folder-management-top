1615e909539ce76ae6a68e67258965e10d096d68
mimisha25 <49067055+mimisha25@users.noreply.github.com>
2025-03-20 14:06:42 -0400

    Feat sign in rendering

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -5,6 +5,9 @@ const prisma = require('../prisma-config')
 const checkAuth = require('../utils/auth');
 const catchAsync = require('../utils/catchAsync');
 
-
+dashboardRouter.route('/register')
+    .get((req, res) => {
+        res.render('auth', { link: '/login', action: '/register', linkT: 'Log In', header: 'Sign In' });
+    })
 
 module.exports = dashboardRouter;
