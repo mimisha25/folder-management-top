38a355dc1a1fcac461ac8143a3055376d87cc0ca
mimisha25 <49067055+mimisha25@users.noreply.github.com>
2025-03-20 14:12:04 -0400

    require the dashboard route

--- app.js
+++ app.js
@@ -53,13 +53,13 @@ app.use((err, req, res, next) => {
 
 })
 
-// const dashboardRouter = require('./routes/dashboard');
+const dashboardRouter = require('./routes/dashboard');
 // const foldersRouter = require('./routes/folder');
 // const filesRouter = require('./routes/files');
 // const homeRouter = require('./routes/home');
 // const uploadRouter = require('./routes/upload');
 
-// app.use('/', dashboardRouter);
+app.use('/', dashboardRouter);
 // app.use('/', foldersRouter);
 // app.use('/', filesRouter);
 // app.use('/', homeRouter);
