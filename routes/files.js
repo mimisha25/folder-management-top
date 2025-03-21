4529bbaae1e1b926ad9391c72bb1ded41de1073b
mimisha25 <49067055+mimisha25@users.noreply.github.com>
2025-03-21 14:33:42 -0400

    Feat rout Read all files and display them

--- routes/files.js
+++ routes/files.js
@@ -3,6 +3,15 @@ const filesRouter = Router();
 const prisma = require('../prisma-config')
 const checkAuth = require('../utils/auth');
 
-
+filesRouter.get('/files', checkAuth, async (req, res) => {
+    try {
+        const user = await prisma.user.findUnique({ where: { id: req.session.userId } });
+        const allFiles = await prisma.file.findMany({
+            where: { userId: req.session.userId },
+            include: { folder: true, user: true }
+        });
+        res.render('files/all-files', { allFiles, user, currentPath: '/files' });
+    } catch (error) { res.status(500).send('Error fetching files.') }
+});
 
 module.exports = filesRouter;
