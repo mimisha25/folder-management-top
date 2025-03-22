c91dcb9a77c4f2afb163e7bef5d66c866d560b11
mimisha25 <49067055+mimisha25@users.noreply.github.com>
2025-03-22 17:18:54 -0400

    Fix bug: change conflicting path

--- routes/home.js
+++ routes/home.js
@@ -3,11 +3,11 @@ const homeRouter = Router();
 const prisma = require('../prisma-config')
 
 
-homeRouter.use('/', async (req, res) => {
+homeRouter.use('/home', async (req, res) => {
     try {
         let user = null;
         if (req.session.userId) user = await prisma.user.findUnique({ where: { id: req.session.userId } });
-        res.render('home', { user })
+        res.render('home', { user, currentPath: '/home' })
     } catch (error) { res.status(500).send('Error fetching folder: ' + error.message); }
 })
 
