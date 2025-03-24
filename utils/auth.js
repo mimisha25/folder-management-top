a6f1d7a52956855f9a14313556c3faba5f8a5420
mimisha25 <49067055+mimisha25@users.noreply.github.com>
2025-03-23 20:16:35 -0400

    Feat auth middleware adn add prisma reading for logged user

--- utils/auth.js
+++ utils/auth.js
@@ -1,7 +1,11 @@
-const checkAuth = (req, res, next) => {
-    if (!req.session.userId) {
-        res.redirect('/login');
-    }
+const catchAsync = require('./catchAsync');
+const prisma = require('../prisma-config')
+
+
+const checkAuth = catchAsync(async (req, res, next) => {
+    if (!req.session.userId) res.redirect('/login');
+    const user = await prisma.user.findUnique({ where: { id: req.session.userId } });
+    req.user = user;
     next();
-};
+});
 module.exports = checkAuth;
