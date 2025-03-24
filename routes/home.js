2655938a95dca647ee3a98a0a8b1ca074260c05e
mimisha25 <49067055+mimisha25@users.noreply.github.com>
2025-03-23 20:14:59 -0400

    Add req user, let unauthorized users also see home page

--- routes/home.js
+++ routes/home.js
@@ -1,14 +1,13 @@
 const { Router } = require('express');
 const homeRouter = Router();
 const prisma = require('../prisma-config')
+const catchAsync = require('../utils/catchAsync');
 
 
-homeRouter.use('/home', async (req, res) => {
-    try {
-        let user = null;
-        if (req.session.userId) user = await prisma.user.findUnique({ where: { id: req.session.userId } });
-        res.render('home', { user, currentPath: '/home' })
-    } catch (error) { res.status(500).send('Error fetching folder: ' + error.message); }
-})
+homeRouter.use('/home', catchAsync(async (req, res) => {
+    let user = null;
+    if (req.session.userId) user = await prisma.user.findUnique({ where: { id: req.session.userId } });
+    res.render('home', { user, currentPath: '/home' })
+}))
 
 module.exports = homeRouter;
