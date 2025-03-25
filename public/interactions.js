1156c1cf26405d124155df96c2b9896476969829
mimisha25 <49067055+mimisha25@users.noreply.github.com>
2025-03-24 20:34:52 -0400

    Feat unique interaction public js file

---
+++ public/interactions.js
@@ -0,0 +1,27 @@
+document.querySelector('.update').addEventListener('click', (event) => {
+    var myModal = new bootstrap.Modal(document.getElementById('myModal'));
+    myModal.show();
+});
+
+
+function copyLink(link) {
+    navigator.clipboard.writeText(link).then(function () {
+        document.getElementById('copyMessage').style.display = 'block';
+        setTimeout(function () {
+            document.getElementById('copyMessage').style.display = 'none';
+        }, 2000);
+    }).catch(function (err) {
+        console.error('Error copying link: ', err);
+    });
+}
+
+
+
+document.getElementById("file-input").addEventListener("change", function () {
+    let fileName = this.files[0].name;
+    let icon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2-circle" viewBox="0 0 16 16">
+<path d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0"/>
+<path d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0z"/>
+</svg>`;
+    document.getElementById("file-name").innerHTML = fileName + " " + icon;
+});
